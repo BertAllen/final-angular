739928d984bdaeab8776762c4f68d1f139f95489
Bert Allen <mrlaughingdragon@aol.com>
2016-03-23 18:56:24 -0600

    90% functional

--- app/artCollector.js
+++ app/artCollector.js
@@ -1,7 +1,14 @@
 app.controller("ArtCollector", function($scope) {
 
-    
-  
+    $scope.gallery = [];
+    $scope.myGreatArt = function(art) {
+        $scope.gallery.push(art);
+        $scope.art = {};
+  } 
+
+    $scope.sellArt = function(index) {
+        $scope.gallery.splice(index, 1);
+}
   
     
 })
